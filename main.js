77cd71517b02639759582d0e98fe39dfd970a184
ReahKy23 <93449477+ReahKy23@users.noreply.github.com>
2024-12-18 12:00:25 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -12,13 +12,4 @@ $(document).mousemove(function(e) {
   $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
 }).mouseover();
 
-function toggleFullscreen(element) {
-  if (!document.fullscreenElement) {
-    element.requestFullscreen().catch(err => {
-      console.error(`Error attempting to enable fullscreen mode: ${err.message} (${err.name})`);
-    });
-  } else {
-    document.exitFullscreen();
-  }
-}
 
